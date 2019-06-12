faaa206056947766c908ea049f02d75fae899427
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-12 10:59:00 +1000

    Solution 1.12 exercise

--- estruturaSequencial/sequencial02.js
+++ estruturaSequencial/sequencial02.js
@@ -15,8 +15,26 @@ console.log(playWithNumbers(2, 2, 2));
 // 1.12 Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que
 // calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
 
-const idealWeight = (height) => {
+const weight = (height) => {
   let weight = (72.7* height) - 58;
   return `Com essa altura, ${height}, o seu peso ideal é ${weight.toFixed(2)}.`;
 };
-console.log(idealWeight(1.68));
+console.log(weight(1.68));
+
+// 1.13 Tendo como dados de entrada a altura e o sexo de uma pessoa, construa um
+// algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+// * a. Para homens: (72.7*h) - 58
+// * b. Para mulheres: (62.1*h) - 44.7 (h = altura)
+// * c. Peça o peso da pessoa e informe se ela está dentro, acima ou abaixo do peso.
+
+const idealWeight = (height, sex) => {
+  if (sex === "M") {
+    const weight = (72.7 * height) - 58;
+    return `Weight, ${sex}: ${weight.toFixed(2)}`;
+  } else ( sex === "F"); {
+    const weight = (62.1 * height) - 44.7;
+    return `Weight, ${sex}: ${weight.toFixed(2)}`;
+  }
+};
+console.log(idealWeight(1.68, "F"));
+console.log(idealWeight(1.72, "M"));
