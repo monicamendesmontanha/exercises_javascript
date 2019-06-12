d89cb43141f80a71aa190f15bb0bc877e0484384
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-12 12:41:25 +1000

    Solution 1.15 exercise (salary)

--- estruturaSequencial/sequencial02.js
+++ estruturaSequencial/sequencial02.js
@@ -72,3 +72,39 @@ console.log(papoDePescador(55));
 
 
 console.log("-----------------------------------------------------------------");
+// 1.15 Faça um Programa que pergunte quanto você ganha por hora e o número de horas
+// trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês,
+// sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e
+// 5% para o sindicato, faça um programa que nos dê:
+
+// * a. salário bruto.
+// * b. quanto pagou ao INSS.
+// * c. quanto pagou ao sindicato.
+// * d. o salário líquido.
+// * e. calcule os descontos e o salário líquido, conforme a tabela abaixo:
+
+
+// + Salário Bruto : R$
+// - IR (11%) : R$
+// - INSS (8%) : R$
+// - Sindicato ( 5%) : R$
+// = Salário Liquido : R$
+
+//  Obs.: Salário Bruto - Descontos = Salário Líquido.
+
+const salary = (earnPerHour, amountWorkhoursPerMonth) => {
+  const salarioBruto = earnPerHour * amountWorkhoursPerMonth;
+  const ir = salarioBruto * 0.11
+  const inss = salarioBruto * 0.08
+  const sindicato = salarioBruto * 0.05
+  const salarioLiquido = salarioBruto - ir - inss - sindicato
+
+  return `
+  - Salário Bruto: $${salarioBruto}
+  - IR (11%): $${ir}
+  - INSS (8%): $${inss}
+  - Sindicato (5%): $${sindicato}
+  - Salário líquido: $${salarioLiquido}
+  `
+}
+console.log(salary(30, 160));
