050fe29ee3642b85f1557401b152c80e3e04dafa
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-28 13:37:54 +1000

    Solution 2.6 exercise

--- estruturaDeDecisao/decisao02.js
+++ estruturaDeDecisao/decisao02.js
@@ -23,3 +23,23 @@ const mediaDoAluno = (nota1, nota2) => {
 console.log(mediaDoAluno(10, 10));
 console.log(mediaDoAluno(9.9, 8.8));
 console.log(mediaDoAluno(4.4, 6.6));
+
+
+
+console.log("-----------------------------------------------------------------------------------------");
+console.log("----------[Question 2.6]-----------------------------------------------------------------");
+// 2.6 Faça um Programa que leia três números e mostre o maior deles.
+
+const maiorNumero = (num1, num2, num3) => {
+  if (num1 > num2 && num1 > num3) {
+    return `${num1} é o maior número.`
+  } else if (num2 > num1 && num2 > num3) {
+    return `${num2} é o maior número.`
+  } else {
+    return `${num3} é o maior número.`
+  }
+}
+
+console.log(maiorNumero(5, 4, 3)); // 5
+console.log(maiorNumero(2, 50, 15)); // 50
+console.log(maiorNumero(1, 10, 150)); //150
