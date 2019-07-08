af98185363be05e93fe442590111c2bd719ed685
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-09 09:50:42 +1000

    Solution exercise 06 - for loop basic

---
+++ repetitionStructure/repetition06.js
@@ -0,0 +1,12 @@
+console.log(
+  "----------[Question 6]----------------------------------------------------------------"
+);
+// 6. Make a program that prints on the screen the numbers from 1 to 20, one below the other.
+
+let number = 0
+
+for (let i=1 ; i <= 20; i++) {
+  console.log(number)
+  number = number + 1
+}
+console.log(number)
