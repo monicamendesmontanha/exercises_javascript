032844958fbe0e0bba37cded501e5a4c81180b67
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-10 14:33:47 +1000

    Fix to show the factorial number

--- repetitionStructure/repetition16.js
+++ repetitionStructure/repetition16.js
@@ -3,22 +3,19 @@ console.log(
 );
 // 17. Make a program that calculates the factorial of a user-supplied integer. Ex: 5! = 5.4.3.2.1 = 120
 
-
 const factorial = (currentNumber) => {
+  let number = currentNumber
   let index =  currentNumber - 1
-  let result
 
   while (index >= 1) {
-    let result = index * currentNumber
-
+    let result = currentNumber * index
     // console.log( `${currentNumber} * ${index} = ${result}` )
 
     currentNumber = result
     index = index - 1
 
   }
-  // console.log(newResult)
-  return `The factorial is ${currentNumber}`
+  return `The factorial of ${number} is ${currentNumber}`
 }
 
 console.log(factorial(5));
